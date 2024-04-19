423d5d9c5f847019139992f6a0b86064c65b9d29
NiuBi02540311 <qhjio@163.com>
2024-04-19 21:50:08 +0800

    21.50

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -40,6 +40,7 @@ namespace WindowsFormsApplication1
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Login
             // 
